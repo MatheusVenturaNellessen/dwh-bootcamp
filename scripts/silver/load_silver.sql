90bdbdb47a6f940ffc24ec6c9e35491c20b27348
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2026-09-24 17:08:09 -0300

    feat: add SQL for insert data into silver.erp_cust_az12

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -24,17 +24,16 @@ SELECT	cst_id,
 			ELSE 'n/a'
 		END AS cst_gndr,
 		cst_create_date
-FROM (
-	SELECT
-		*,
-        ROW_NUMBER() OVER (
-        	PARTITION BY cst_id
-        	ORDER BY cst_create_date DESC
-        ) AS flag
-    FROM bronze.crm_cust_info
-    WHERE cst_id IS NOT NULL
+FROM 	(
+	SELECT	*,
+        	ROW_NUMBER() OVER (
+        		PARTITION BY cst_id
+        		ORDER BY cst_create_date DESC
+        	) AS flag
+    FROM 	bronze.crm_cust_info
+    WHERE 	cst_id IS NOT NULL
 )t
-WHERE flag = 1;
+WHERE 	flag = 1;
 
 TRUNCATE TABLE silver.crm_prd_info;
 
@@ -105,3 +104,22 @@ SELECT	sls_ord_num,
 			ELSE sls_price
 		END AS sls_price
 FROM	bronze.crm_sales_details;
+
+TRUNCATE TABLE silver.erp_cust_az12;
+
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)
+SELECT	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+			 ELSE cid
+		END  AS cid,
+		CASE WHEN bdate > CURRENT_DATE THEN NULL
+			 ELSE bdate
+		END  AS bdate,
+		CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+			 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+			 ELSE 'n/a'
+		END  AS gen
+FROM	bronze.erp_cust_az12;
